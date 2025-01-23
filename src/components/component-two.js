b6ec5039723a968af84f12807230c5356dc38b55
a1000563 <159111772+a1000563@users.noreply.github.com>
2025-01-24 03:00:17 +0800

    Update component-two.js

--- src/components/component-two.js
+++ src/components/component-two.js
@@ -2,12 +2,6 @@ import React from 'react'
 import {
     CContainer,
     CRow,
-    CCol,
-    CCard,
-    CCardBody,
-    CCardTitle,
-    CCardText,
-    CButton,
   } from '@coreui/react';
   import { BarChart } from './bar-chart';
 
